691217a4cf2956175b06938eb979f6d509ee3b0f
hunt32 <hunt32@gmail.com>
2015-06-22 00:42:50 +0200

    solved horse racing

---
+++ HorseRacingDuals/README.md
@@ -0,0 +1,4 @@
+CodinGame
+==========
+
+<a href="https://www.codingame.com/ide/14976146cce19a7d32e63a540d955323f5d46f2">Horse-Racing Duals</a>
---
+++ HorseRacingDuals/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] P = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            P[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(P);
+        int min = P[1] - P[0];
+        for (int i = 2; i != P.Length; i++)
+        {
+            min = Math.Min(min, P[i] - P[i - 1]);
+        }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine(min);
+    }
+}
